9d8f56801c8be24b298f66bfdd7f2e206ecdbc0a
Tomislav Haus <tomislav.haus@gmail.com>
2017-12-13 11:21:48 +0100

    Added attitude controller which utilizes only u3 and u4 commands. Added pid class. Added reference publisher.

---
+++ src/attitude_ref_publisher.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+__author__ = 'thaus'
+
+import rospy
+from geometry_msgs.msg import Vector3
+from std_srvs.srv  import Empty
+
+class ReferencePublisher(object):
+    """docstring for ReferencePublisher"""
+    def __init__(self):
+        
+        self.euler_ref_pub = rospy.Publisher('euler_ref', Vector3, queue_size=1)
+        self.euler_ref_msg = Vector3()
+        self.pitch_ref = [0, 0.2, -0.2, 0, 0 , 0 ,0]
+        self.roll_ref = [0, 0.0, -0.0, 0, 0.2 , -0.2, 0]
+        self.counter = 0
+
+    def run(self):
+
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            gazebo_start = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+            gazebo_start()
+            
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        while not rospy.is_shutdown():
+
+            rospy.sleep(5.0)
+            self.euler_ref_msg.x = self.roll_ref[self.counter]
+            self.euler_ref_msg.y = self.pitch_ref[self.counter]
+            self.euler_ref_msg.z = 0
+
+            self.euler_ref_pub.publish(self.euler_ref_msg)
+
+            self.counter = (self.counter + 1 ) % len(self.pitch_ref)
+
+if __name__ == '__main__':
+
+    rospy.init_node('attitude_ref_pub')
+    attitude_ref = ReferencePublisher()
+    attitude_ref.run()
